1e9ad0515a9d682e5fe50c15b18dca267153c10b
josiaspegueroltjnc <josiaspegueroltjnc@gmail.com>
2025-09-29 22:59:17 -0400

    modify DTOs

--- EFCore/EFCore/DTOs/PeliculaActorCreacionDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace EFCore.DTOs
-{
-    public class PeliculaActorCreacionDTO
-    {
-        public int ActorId { get; set; }
-        public string Personaje { get; set; } = string.Empty;
-    }
-}
